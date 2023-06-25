1d57ffe20e7723aafc52a6542874796dd4d2cfa6
kavandalal <kavandalal003@gmail.com>
2023-06-25 09:35:03 +0530

    added particles in not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,11 @@
+import Particles from '@/ui/particles';
 import Link from 'next/link';
 
 export default function NotFound() {
 	return (
-		<div className='text-xl space-y-9 text-center flex justify-center items-center flex-col min-h-screen'>
-			<h1 className='text-4xl  text-vercel-red'>Not Found</h1>
+		<div className='text-xl space-y-9 text-center flex justify-center items-center flex-col min-h-screen text-white'>
+			<Particles className='absolute inset-0 -z-10 animate-fade-in' quantity={100} />
+			<h1 className='text-4xl  text-vercel-red'>Page Not Found</h1>
 			<p className=''>WHY ARE YOU RUNNING !!!</p>
 			<p className=''>
 				<Link href='/' className='text-vercel-blue' replace>
